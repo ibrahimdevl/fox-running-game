b68389077de2c0ce89e8548561587c976b64d8ea
ibrahimdevl <ibrahimhoussem2000@gmail.com>
2026-01-18 15:44:04 +0100

    Add camera background color setting in CameraController

--- runningball/Assets/Scripts/CameraController.cs
+++ runningball/Assets/Scripts/CameraController.cs
@@ -7,9 +7,18 @@ public class CameraController : MonoBehaviour
     [SerializeField]
     private Transform target;
     private float zDistance;
+    private Camera mainCamera;
 
     private void Awake()
     {
+        mainCamera = GetComponent<Camera>();
+        
+        // Set a nice sky blue background color
+        if (mainCamera != null)
+        {
+            mainCamera.backgroundColor = new Color(0.5f, 0.8f, 1.0f, 1.0f); // Sky blue
+        }
+
         if( target != null )
         {
             zDistance = target.position.z - transform.position.z;
